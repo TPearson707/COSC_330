f8559a5c00e8367d43c812a05543501791720f1d
Thomas Pearson <pearson.e.thomas@gmail.com>
2025-02-01 17:49:18 -0500

    finished the base Tic Tac Toe implementation in java, need to add the network portion

---
+++ Lab1/TicTacToe/TicTacToe.java
@@ -0,0 +1,63 @@
+public class TicTacToe {
+    private char[][] board;
+
+
+    public TicTacToe() {
+        board = new char[3][3];
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public boolean checkWin() {
+
+        // Check rows for a win
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+                return true;
+            }
+        }
+
+        // Check columns for a win
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] != '-' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
+                return true;
+            }
+        }
+
+        // Check the first diagonal for a win
+        if (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        } 
+
+        // Check the second diagonal for a win
+        if (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+            return true;
+        }
+
+        // no winner
+        return false;
+    }
+
+    public boolean setBoard(char letter, int x, int y) {
+        if (board[x][y] == '-') {
+            board[x][y] = letter;
+            return true;
+        } 
+        
+        return false;
+        
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
